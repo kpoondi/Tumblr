800697e5074bfeaa3fe1b04a6c35c80a03e5f92a
Kausthub Poondi <kpoondi@wireless-10-145-32-152.public.utexas.edu>
2017-02-08 19:31:56 -0600

    first commit

---
+++ Tumblr/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  Tumblr
+//
+//  Created by Kausthub Poondi on 2/8/17.
+//  Copyright © 2017 Kausthub Poondi. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Tumblr/PhotoCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCellTableViewCell.swift
+//  Tumblr
+//
+//  Created by Kausthub Poondi on 2/8/17.
+//  Copyright © 2017 Kausthub Poondi. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
